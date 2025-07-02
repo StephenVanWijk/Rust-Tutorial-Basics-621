ed08772af696d15ded85641a0d0b1c9841b8ad68
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-07-02 12:48:08 +0200

    modified Cargo.toml Core_Memory_Safety_Performance_Concepts/memory_management/src/lib.rs Getting_Started_Basics/variables_number_strings_and_bools/Cargo.toml new file Core_Memory_Safety_Performance_Concepts/memory_management/src/main.rs renamed project-setup-Rust-Tutorial-Basics-621.ps1 -> Getting_Started_Basics/initializing_a_project_locally/src/project-setup-Rust-Tutorial-Basics-621.ps1 2050702 1247CET SDvW

--- Core_Memory_Safety_Performance_Concepts/memory_management/src/lib.rs
+++ Core_Memory_Safety_Performance_Concepts/memory_management/src/lib.rs
@@ -1,1 +1,4 @@
 // Memory management library root
+pub fn print_usize_size() {
+    println!("usize is {} bytes", std::mem::size_of::<usize>());
+}
---
+++ Core_Memory_Safety_Performance_Concepts/memory_management/src/main.rs
@@ -0,0 +1,4 @@
+use memory_management::print_usize_size;
+fn main() {
+    print_usize_size();
+}
