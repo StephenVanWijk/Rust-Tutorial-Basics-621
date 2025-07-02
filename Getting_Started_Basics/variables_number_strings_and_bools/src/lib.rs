cfc995cbde5c49b70c1908f7010dfc7b78d95616
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-07-02 10:19:19 +0200

    Add lib.rs and main.rs for variables demo; update workspace Cargo.toml files; remove project setup script 20250702 1020CET SDvW

---
+++ Getting_Started_Basics/variables_number_strings_and_bools/src/lib.rs
@@ -0,0 +1,6 @@
+#[allow(dead_code)]
+
+pub fn simple_var_example (){
+    let x: i8 = 5;
+    println!("Variable x: {}", x);    
+}
---
+++ Getting_Started_Basics/variables_number_strings_and_bools/src/main.rs
@@ -0,0 +1,5 @@
+use variables_number_strings_and_bools::simple_var_example;
+fn main() {
+    // Call the function to demonstrate variable usage
+    simple_var_example();
+}
