b0e739254bed4a2c1bdb1138fe2abab70c0d171d
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-07-04 14:55:56 +0200

    modified: src/lib.rs src/main.rs 20250704 1455CET SDvW

--- Getting_Started_Basics/conditionals_loops/src/lib.rs
+++ Getting_Started_Basics/conditionals_loops/src/lib.rs
@@ -29,6 +29,28 @@ pub fn if_in_let_statement_type_error(){
     // println!("The value of number is: {number}");
 }
 
+pub fn loop_labels(){
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
 pub fn returning_values_from_loops(){
     let mut counter = 0;
     let result = loop {
@@ -40,4 +62,61 @@ pub fn returning_values_from_loops(){
     };
 
     println!("The result is: {}", result);
+}
+
+pub fn while_loop_countdown() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+pub fn for_loop_iteration() {
+    let a = [10, 20, 30, 40, 50];
+
+    println!("Iterating through an array:");
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    println!("\nIterating through a range (reversed):");
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+
+pub fn if_let_while_let_demo() {
+    println!("--- `if let` for concise control flow ---");
+    // Using `match` can be verbose if you only care about one variant.
+    // let config_max: Option<i32> = Some(3);
+    let config_max: Option<i32> = None;
+    match config_max {
+        Some(max) => println!("(Using match) The maximum is configured to be {}", max),
+        None => println!("(Using if let/else) No maximum was configured.") , // We have to handle the `None` case, even if we do nothing.
+    }
+
+    // `if let` is a much cleaner way to do the same thing.
+    if let Some(max) = config_max {
+        println!("(Using if let) The maximum is configured to be {}", max);
+    }
+
+    // You can also include an `else` block.
+    let another_config: Option<i32> = None;
+    if let Some(max) = another_config {
+        println!("The maximum is configured to be {}", max);
+    } else {
+        println!("(Using if let/else) No maximum was configured.");
+    }
+
+    println!("\n--- `while let` for conditional loops ---");
+    let mut stack = vec![1, 2, 3];
+    while let Some(top) = stack.pop() {
+        println!("Popped from stack: {}", top);
+    }
+    println!("Stack is now empty.");
 }
--- Getting_Started_Basics/conditionals_loops/src/main.rs
+++ Getting_Started_Basics/conditionals_loops/src/main.rs
@@ -1,6 +1,29 @@
-use conditionals_loops::returning_values_from_loops;
+// use conditionals_loops::{
+//     conditionals_divisible, for_loop_iteration, if_in_let_statement, if_let_while_let_demo,
+//     loop_labels, returning_values_from_loops, while_loop_countdown,
+// };
 
-fn main(){
-    // Call the function to demonstrate returning values from loops
-    returning_values_from_loops();
+// use conditionals_loops::conditionals_divisible;
+// use conditionals_loops::for_loop_iteration;
+// use conditionals_loops::if_in_let_statement;
+use conditionals_loops::if_let_while_let_demo;
+// use conditionals_loops::loop_labels;
+// use conditionals_loops::returning_values_from_loops;
+// use conditionals_loops::while_loop_countdown;
+
+fn main() {
+    // println!("--- Returning Values from Loops ---");
+    // returning_values_from_loops();
+    // println!("\n--- Basic Conditionals ---");
+    // conditionals_divisible();
+    // println!("\n--- If in a Let Statement ---");
+    // if_in_let_statement();
+    // println!("\n--- Loop Labels ---");
+    // loop_labels();
+    // println!("\n--- While Loop ---");
+    // while_loop_countdown();
+    // println!("\n--- For Loop ---");
+    // for_loop_iteration();
+    println!("\n--- `if let` and `while let` ---");
+    if_let_while_let_demo();
 }
